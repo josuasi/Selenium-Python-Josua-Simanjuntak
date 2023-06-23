61c3aafffdd2091a1088ced6458937b9d965516b
josuasi <37526469+josuasi@users.noreply.github.com>
2023-06-23 13:05:59 +0700

    Add files via upload

---
+++ Selenium-Python/Resources/config.py
@@ -0,0 +1,14 @@
+HomePage        = "https://zxing.appspot.com/generator/"
+
+ENV = "WIN"
+
+WebBrowser      = "Chrome"
+
+# HeadStatus, WindowSize  = "--headless", "--window-size=1920,1080"
+HeadStatus, WindowSize  = None, None
+
+
+CodeBuild       = True
+CheckPoint      = 'Your privacy settings'
+
+
